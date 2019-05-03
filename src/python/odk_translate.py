eda8ad87e646de0d0bd30a6e13a08089b262fc32
stevensotelo <stevenbetancurt@hotmail.com>
2019-05-03 07:53:24 -0500

    added method to validated fields. it now checks mandatatory fields and regular expression

--- src/python/odk_translate.py
+++ src/python/odk_translate.py
@@ -2,18 +2,84 @@ import conf as c
 import pandas as pd
 import os
 from os import listdir
+import re
+import numpy as np
 
-## Method whichTrim whitespace from ends of each value across all series in dataframe
+## Method which Trim whitespace from ends of each value across all series in dataframe
 ## (dataframe) df: Dataframe for cleaning
 def trim_all_columns(df):
     trim_strings = lambda x: x.strip() if type(x) is str else x
     return df.applymap(trim_strings)
 
+## Method which apply rules for transforming data into new dataset
+## (dataframe) rules: Table, which has the rules to be applied to dataset
+## (string) table_name: Name of table 
+## (dataframe) data: Dataset which will get the transformations
+def apply_transformations(rules, table_name, data):
+    tmp_data = data
+
+    # Getting rules
+    transformations_tmp = rules[rules["table"] == table_name]
+    
+    ## Replace
+    trs_replace = transformations_tmp[transformations_tmp["type"] == "replace"]
+    if( trs_replace.shape[0] > 0 ):
+        for tmp_field in trs_replace.field.unique():
+            for row in trs_replace[trs_replace["field"] == tmp_field].itertuples(index=True, name='Pandas') :  
+                tmp_data[tmp_field] = tmp_data[tmp_field].str.replace('^' + getattr(row, "value") + '$',getattr(row, "transform"))    
+    ## Split
+    trs_replace = transformations_tmp[transformations_tmp["type"] == "split"]
+    if( trs_replace.shape[0] > 0 ):
+        for tmp_field in trs_replace.field.unique():
+            for row in trs_replace[trs_replace["field"] == tmp_field].itertuples(index=True, name='Pandas') :
+                tmp_data[[getattr(row, "field"),getattr(row, "transform")]] = tmp_data[getattr(row, "field")].str.split(getattr(row, "value"),expand=True)
+
+    return data
+
+## Method which validates that fields are in good shape
+## (dataframe) rules: Table, which has the rules to validate
+## (string) table_name: Name of table 
+## (dataframe) data: Dataset which will be validated
+## (int) p: Number of process
+def get_validations(rules, table_name, data, p):
+    # Preparing data
+    log = data
+    log["ERROR"] = ""
+    
+    # Checking mandatory fields
+    mandatory_tmp = rules[(rules["type"] == "mandatory") & (rules["table"] == table_name)]   
+    
+    for mdt in mandatory_tmp.itertuples(index=True, name='Pandas') :
+        # Without condition                     # According to current process
+        if((getattr(mdt, "condition") == "" or pd.isnull(getattr(mdt, "condition"))) or (getattr(mdt, "condition") == "process" and p == int(getattr(mdt, "condition_value")))):
+            missing_values =  log[getattr(mdt, "field")].isna()
+            if (missing_values[missing_values].shape[0] == 0):
+                missing_values =  log[getattr(mdt, "field")].astype(str) == ""
+            log.loc[missing_values, "ERROR"] = log.loc[missing_values, "ERROR"] + " Empty field: " + table_name + " : " + getattr(mdt, "field") + ","
+    
+    # Regular expressions
+    reg_exp = rules[(rules["type"] == "reg_exp") & (rules["table"] == table_name)]
+    for row in reg_exp.itertuples(index=True, name='Pandas') :
+        # Without condition
+        if(getattr(row, "condition") == "" or pd.isnull(getattr(row, "condition"))):
+            missing_values =  log[getattr(row, "field")].astype(str).str.contains(getattr(row, "expression"), case = True, na=False, regex=True)
+            log.loc[missing_values == False, "ERROR"] = log.loc[missing_values == False, "ERROR"] + "Invalid field (regular expression): " + table_name + " : " + getattr(row, "field") + ", "
+
+    # Saving log of issues
+    log_error = log["ERROR"] != ""
+    if(log[log_error].shape[0] > 0):
+        log[log_error].to_csv(c.path_logs + "validations-" + table_name + ".csv", index = False)    
+    # Getting the data without issues
+    log = log[log_error == False]
+    log.drop('ERROR', axis=1, inplace=True)
+    return log
+
+
 def save(df, keys, path):
     if(df.shape[0] > 0):
         if (os.path.isfile(path)) :
             old =  pd.read_csv(path, dtype=str)
-            df = pd.concat([df,old])
+            df = pd.concat([df,old], sort = False)
 
         # Removing duplicates
         df = df.drop_duplicates(subset = keys, keep = 'last')
@@ -29,7 +95,7 @@ def save(df, keys, path):
 def process_file(process, file, cnn, table_name):
     
     # Getting the fields from 
-    form_fields = ["form_sheet","form_field","form_key","mandatory",table_name]
+    form_fields = ["form_sheet","form_field","form_key",table_name]
     f = form[form["process"] == process]
     form_tmp = f[form_fields]
     form_tmp = form_tmp[form_tmp[table_name].notna()] 
@@ -44,35 +110,20 @@ def process_file(process, file, cnn, table_name):
     data_raw = pd.read_excel(file, sheet_name='Hoja 1')
     
     # Cleaning data with empty spaces
+    print("\t\t\tCleaning data")
     data_raw = trim_all_columns(data_raw)
 
-    # Checking mandatory fields    
-    mandatory_tmp = form_tmp[form_tmp.mandatory == 1]
-    log = data_raw
-    log["ERROR"] = ""
-    for mdt in mandatory_tmp.itertuples(index=True, name='Pandas') :
-        missing_values =  data_raw[getattr(mdt, "form_field")].isna()
-        if (missing_values[missing_values].shape[0] == 0):
-            missing_values =  data_raw[getattr(mdt, "form_field")].astype(str) == ""
-        log.loc[missing_values, "ERROR"] = log.loc[missing_values, "ERROR"] + " Empty field: " + getattr(mdt, table_name) + " : " + getattr(mdt, "form_field") + ","
-        
-    log_error = log["ERROR"] != ""
-    if(log[log_error].shape[0] > 0):
-        log[log_error].to_csv(c.path_logs + "mandatory-" + table_name + ".csv", index = False)
-    
-    # Getting the data raw without issues
-    data_raw = data_raw[log_error == False]
+    # Validating
 
-    # Filtering data duplicates
-    import_data = data_raw[form_tmp.form_field.values]
-        
     # Removing duplicates
+    print("\t\t\tRemoving duplicates")
+    import_data = data_raw[form_tmp.form_field.values]
     import_data = import_data.drop_duplicates(subset = keys.form_field.values, keep = 'last')
 
-    # Getting values from database
+    # Comparing with data of the database
+    print("\t\t\tComparing with data of the database")
     table = pd.read_sql_table(table_name, cnn)
-
-    # Ordering datasets
+    # Ordering datasets    
     import_data = import_data.sort_values(by=keys['form_field'].values.tolist())
     table = table.sort_values(by=keys[table_name].values.tolist())
     table = table[keys[table_name].values]
@@ -82,30 +133,22 @@ def process_file(process, file, cnn, table_name):
     table = table.reset_index(drop=True)
     import_data = import_data.reset_index(drop=True)
 
-    # Applying transformations
-    transformations_tmp = transformations[transformations["table"] == table_name]
-    
-    ## Replace
-    trs_replace = transformations_tmp[transformations_tmp["type"] == "replace"]
-    if( trs_replace.shape[0] > 0 ):
-        for tmp_field in trs_replace.field.unique():
-            for row in trs_replace[trs_replace["field"] == tmp_field].itertuples(index=True, name='Pandas') :  
-                import_data[tmp_field] = import_data[tmp_field].str.replace('^' + getattr(row, "value") + '$',getattr(row, "transform"))    
-    ## Split
-    trs_replace = transformations_tmp[transformations_tmp["type"] == "split"]
-    if( trs_replace.shape[0] > 0 ):
-        for tmp_field in trs_replace.field.unique():
-            for row in trs_replace[trs_replace["field"] == tmp_field].itertuples(index=True, name='Pandas') :
-                import_data[[getattr(row, "field"),getattr(row, "transform")]] = import_data[getattr(row, "field")].str.split(getattr(row, "value"),expand=True)
+    # Transformations
+    print("\t\t\tTransforming data")
+    import_data = apply_transformations(transformations, table_name, import_data)
 
+    # Validations
+    print("\t\t\tValidating data")
+    import_data = get_validations(validations, table_name, import_data, process)
 
-    # Looking for records available into database
+    # Looking for records available into database    
     records_available = import_data.isin(table)
     records = records_available[records_available.columns.values[0]]
     records_new = import_data[records == False]
     records_update = import_data[records == True]    
 
     # Saving records
+    print("\t\t\tSaving")
     save(records_new, keys[table_name].values, c.path_ouputs_new + table_name + ".csv")
     save(records_update, keys[table_name].values, c.path_ouputs_updates + table_name + ".csv")
     return { 'new' : records_new.shape[0], 'updates': records_update.shape[0]  }
@@ -121,6 +164,7 @@ tables = c.tables_master
 # Getting the form structure
 form = pd.read_excel(c.path_form, sheet_name='header')
 transformations = pd.read_excel(c.path_form, sheet_name='transformations')
+validations = pd.read_excel(c.path_form, sheet_name='validations')
 # Getting database connection
 print("Connecting database")
 db_connection = c.connect_db()
