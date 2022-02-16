6b6cb8bce97bbd2da2b58b86667c57b6cbd605fc
stevensotelo <stevenbetancurt@hotmail.com>
2022-02-16 16:30:48 -0500

    fixed

--- src/helpers/configure-survey.sql
+++ src/helpers/configure-survey.sql
@@ -1,3 +1,5 @@
-select b.name,b.repeat,q.id,q.name,q.type
+select f.id,f.name,b.name,b.repeat,q.id,q.name,q.type
 from frm_questions as q
-	inner join frm_blocks as b on q.block = b.id;
+	inner join frm_blocks as b on q.block = b.id
+    inner join frm_blocks_forms as bf on bf.block = b.id
+    inner join frm_forms as f on bf.form = f.id;
